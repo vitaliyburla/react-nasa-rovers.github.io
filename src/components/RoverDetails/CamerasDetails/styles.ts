8d37ad5403e67d0ab1990b1cc5ce233927f6907f
Vitaliy Burla <v.burla96@gmail.com>
2022-03-22 16:41:44 +0200

    camera card styles fixed

--- src/components/RoverDetails/CamerasDetails/styles.ts
+++ src/components/RoverDetails/CamerasDetails/styles.ts
@@ -2,9 +2,4 @@ import { Theme } from '@mui/material';
 import { blueGrey, blue } from '@mui/material/colors';
 import { makeStyles } from '@mui/styles';
 
-export const useStyles = makeStyles((theme: Theme) => ({
-    cameraCard: {
-        background: theme.palette.secondary.light,
-        color: theme.palette.common.white,
-    },
-}));
+export const useStyles = makeStyles((theme: Theme) => ({}));
