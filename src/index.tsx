59cf91bd8923ec517bf1e37fd3e2a3db878bc1ce
Vitaliy Burla <v.burla96@gmail.com>
2022-04-23 14:29:15 +0300

    router provider changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './store';
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './theme';
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </ThemeProvider>,
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>,
     document.getElementById('root')
 );
--- src/theme.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-import { blueGrey } from '@mui/material/colors';
-
-export const theme = createTheme({
-    palette: {
-        primary: blueGrey,
-    },
-});
